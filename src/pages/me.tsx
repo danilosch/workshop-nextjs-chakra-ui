3d7d22be5d1ec7a7b76e46d84fcf7aa7aee715e9
Danilo Schervinski <danilo_sch@live.com>
2022-07-30 17:57:17 -0300

    chore: Profile page refactoring

--- src/pages/me.tsx
+++ src/pages/me.tsx
@@ -1,31 +1,49 @@
-import { GetServerSideProps } from "next";
 import ProfileForm from "@/components/forms/ProfileForm";
 import {
   FormValues,
   ProfileFormRefType,
 } from "@/components/forms/ProfileForm/ProfileForm";
 import PrivatePage from "@/layouts/PrivatePage";
-import { Container } from "@chakra-ui/react";
-import React, { ReactElement, useRef } from "react";
-import { SubmitHandler } from "react-hook-form";
-import { NextPageWithLayout } from "./_app";
 import api, { getAPIClient, httpErrorHandler } from "@/services/api";
 import { removeEmptyValues } from "@/utils/parse";
+import { Container } from "@chakra-ui/react";
 import omit from "lodash.omit";
+import { GetServerSideProps } from "next";
+import React, { ReactElement, useRef } from "react";
+import { SubmitHandler } from "react-hook-form";
 import { toast } from "react-toastify";
+import { NextPageWithLayout } from "./_app";
 
-const Me: NextPageWithLayout = ({ data }) => {
+type Me = {
+  created_at: string;
+  email: string;
+  id: string;
+  name: string;
+  remember_me_token: null;
+  status: boolean;
+  updated_at: string;
+};
+
+type PageProps = {
+  data: Me;
+};
+
+const Me: NextPageWithLayout<PageProps> = ({ data }) => {
   const formRef = useRef<ProfileFormRefType>(null);
 
   const onSubmit: SubmitHandler<FormValues> = async (values) => {
     removeEmptyValues(values);
+
     const apiValues = omit(values, ["id", "created_at", "confirmPassword"]);
+
     try {
-      await api.put(`/users/${values.id}`, apiValues);
+      await api.put(`users/${data.id}`, apiValues);
     } catch (error) {
       httpErrorHandler(error, formRef.current?.setError);
     }
+
     toast.success("Data edited successfully!");
+
     formRef.current?.setValue("password", "");
     formRef.current?.setValue("confirmPassword", "");
     (document.activeElement as HTMLElement).blur();
